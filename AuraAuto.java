14387c501fb54d7af46aae3ead3c72ff6ad2c277
Bob-3737 <max.m.todd4@gmail.com>
2026-04-23 15:44:43 -0600

    Update AuraAuto.java
    for redBack

--- AuraAuto.java
+++ AuraAuto.java
@@ -2,8 +2,7 @@
 
 //import all the things
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
-import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -14,7 +13,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous
 
 //declare the class
-public class auraAuto extends LinearOpMode {
+public class redBackAuto extends LinearOpMode {
 
     //declare the opmode that can be interrupted
      @Override
@@ -27,63 +26,106 @@ public class auraAuto extends LinearOpMode {
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("BL");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("FR");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("BR");
-        
-        //declare our color sensor
-        ColorSensor auraColorSensor = hardwareMap.colorSensor.get("CO");
-        
-        //set our color sensor variables
-        int red = auraColorSensor.red();
-        int blue = auraColorSensor.blue();
+        DcMotor intakeMotor = hardwareMap.dcMotor.get("IM");
+        DcMotor frontleftShooter = hardwareMap.dcMotor.get("S1");
+        DcMotor frontrightShooter = hardwareMap.dcMotor.get("S2");
         
         //reverse all them motors
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        Servo stopper = hardwareMap.servo.get("COCK(blocker)");
+        
+        CRServo BRS = hardwareMap.crservo.get("BRS");
+        CRServo BLS = hardwareMap.crservo.get("BLS");
+        CRServo FRS = hardwareMap.crservo.get("FRS");
+        CRServo FLS = hardwareMap.crservo.get("FLS");
+        
+        frontrightShooter.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        BLS.setDirection(DcMotorSimple.Direction.REVERSE);
+        FLS.setDirection(DcMotorSimple.Direction.REVERSE);
+        
         
         
         //wait fo thu start
         waitForStart();
         
-        sleep(2000);
-        while (opModeIsActive()) {
-            //set color sensor data to variables
-            red = auraColorSensor.red();
-            blue = auraColorSensor.blue();
-            
-            //send color sensor data to drive station
-            telemetry.addData("r", red);
-            telemetry.addData("b", blue);
-            
-            //recieve it
-            telemetry.update();
-            
-            //set paur
-            if (red > blue)  {
-                frontLeftMotor.setPower(0.5);
-                backLeftMotor.setPower(0.5);
-                frontRightMotor.setPower(0.5);
-                backRightMotor.setPower(0.5);
-            } else {
-                frontLeftMotor.setPower(-0.5);
-                backLeftMotor.setPower(-0.5);
-                frontRightMotor.setPower(-0.5);
-                backRightMotor.setPower(-0.5);
-            }
-            break;
-        }
+        double shootPower = (0);
+
+        //first auto function
+        frontLeftMotor.setPower(1);
+        backLeftMotor.setPower(1);
+        frontRightMotor.setPower(1);
+        backRightMotor.setPower(1);
+ 
         
-        //wait for 300 ms before turning off motors
-        sleep(300);
+         //wait for 300 ms before turning off motors
+        sleep(70);
         
-        //turn off motors
         frontLeftMotor.setPower(0);
         backLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backRightMotor.setPower(0);
         
+        sleep(1000);
         
+        frontLeftMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(-0.5);
+        backRightMotor.setPower(-0.5);
+        
+        shootPower = 0.57;
+        
+        sleep(110);
+         
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        
+        frontleftShooter.setPower(shootPower);
+        frontrightShooter.setPower(shootPower);
+        
+        intakeMotor.setPower(1);
+        
+        BRS.setPower(1);
+        BLS.setPower(1);
+        FRS.setPower(1);
+        FLS.setPower(1);
+
+        sleep(8000);
+        
+        stopper.setPosition(50);
+        
+        sleep(3000);
+        
+        //turn off all motors
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        
+        BRS.setPower(0);
+        BLS.setPower(0);
+        FRS.setPower(0);
+        FLS.setPower(0);
+        
+        frontleftShooter.setPower(0);
+        frontrightShooter.setPower(0);
+    
+        intakeMotor.setPower(0);
+
     }
+    
 }
 
 
+
+
+
+        
+        
+    }
+}
